4010fa33d9103a8313c1f6c1aea2d503782ce05a
@svenkataraman108 <kpvenkat108@gmail.com>
2024-05-22 15:29:42 -0500

    Initial Commit

---
+++ iterationtodo/iterationtodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  iterationtodoApp.swift
+//  iterationtodo
+//
+//  Created by Scholar on 5/22/24.
+//
+
+import SwiftUI
+
+@main
+struct iterationtodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
